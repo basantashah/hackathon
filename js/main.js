df79e0bc910dd604132085735c868fb4aa07ba39
basantashah <bindassbasanta@hotmail.com>
2018-07-29 16:21:47 +0545

    changes

--- js/main.js
+++ js/main.js
@@ -208,4 +208,11 @@
 
 	init();
 
-})();
+})
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+	img.setAttribute('src', img.getAttribute('data-src'));
+	img.onload = function () {
+		img.removeAttribute('data-src');
+	};
+});
+();
